f79cf00660f47c7114666b7afcaf089de4f46981
伊藤 駿 <rendya2501@gmail.com>
2026-01-04 23:50:05 +0900

    非同期化

--- VideoGameApiVsa/Data/VideoGameDbContextSeed.cs
+++ VideoGameApiVsa/Data/VideoGameDbContextSeed.cs
@@ -16,14 +16,14 @@ public static class VideoGameDbContextSeed
     {
         if (!context.VideoGames.Any())
         {
-            context.VideoGames.AddRange(
+            await context.VideoGames.AddRangeAsync(
                 new VideoGame { Id = 1, Genre = "Action", Title = "The Legend of Zelda: Breath of the Wild", ReleaseYear = 2017 },
                 new VideoGame { Id = 2, Genre = "RPG", Title = "The Witcher 3: Wild Hunt", ReleaseYear = 2015 },
                 new VideoGame { Id = 3, Genre = "Shooter", Title = "DOOM Eternal", ReleaseYear = 2020 },
                 new VideoGame { Id = 4, Genre = "Adventure", Title = "Red Dead Redemption 2", ReleaseYear = 2018 },
                 new VideoGame { Id = 5, Genre = "Strategy", Title = "Civilization VI", ReleaseYear = 2016 }
             );
-            context.SaveChanges();
+            await context.SaveChangesAsync();
         }
     }
 }
